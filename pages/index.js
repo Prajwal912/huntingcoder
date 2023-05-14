5c020ee8cbf7e546a1184cbfa31af52e55ebd1b2
prajwal <prajwalshah912@gmail.com>
2023-05-14 18:05:07 +0530

    basic cleanup done in home page

--- pages/index.js
+++ pages/index.js
@@ -2,38 +2,49 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Link from "next/link";
-import Style from "../styles/home.module.css"
+import Style from "../styles/home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <style jsx>
-  {`
-  .dummy{
-    font-size: 1.875rem;
-    line-height: 2.25rem;
-    text-align:center;
-    font-weight:600;
-    text-decoration:underline;
-    margin-bottom: 1.25rem;
-    // background:red;
-    // color:white
-  }
-  `}
-    </style>
+      <style jsx>
+        {`
+          .dummy {
+            font-size: 1.875rem;
+            line-height: 2.25rem;
+            text-align: center;
+            font-weight: 600;
+            text-decoration: underline;
+            margin-bottom: 1.25rem;
+            // background:red;
+            // color:white
+          }
+        `}
+      </style>
 
-
-      <header className="text-gray-600 body-font" >
+      <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-            <Link href='/' className="mr-5 text-black hover:text-gray-900 font-bold">
+            <Link
+              href="/"
+              className="mr-5 text-black hover:text-gray-900 font-bold"
+            >
               Home
             </Link>
-            <Link href='/about' className="mr-5 hover:text-gray-900 font-bold">About</Link>
-            <Link href='/blog' className="mr-5 hover:text-gray-900 font-bold">Blog</Link>
-            <Link href='/contact' className="mr-5 hover:text-gray-900 font-bold">Contact</Link>
+            <Link href="/about" className="mr-5 hover:text-gray-900 font-bold">
+              About
+            </Link>
+            <Link href="/blog" className="mr-5 hover:text-gray-900 font-bold">
+              Blog
+            </Link>
+            <Link
+              href="/contact"
+              className="mr-5 hover:text-gray-900 font-bold"
+            >
+              Contact
+            </Link>
           </nav>
         </div>
       </header>
@@ -50,15 +61,14 @@ export default function Home() {
         className={`flex min-h-screen flex-col items-center space-between p-10 ${inter.className}`}
       >
         <h1 className={`text-5xl font-semibold mb-5`}>Hunting Coders</h1>
+        <Image src={"/home-img.jpg"} width={200} height={100} alt="" className="my-5 rounded-[10px]"></Image>
 
-        <p className=" flex w-full justify-center lg:w-auto mb-8">
+        <p className="flex w-full justify-center lg:w-auto mb-8 font-semibold">
           A blog for hunting coders by a Hunting Coders
         </p>
 
         <div className="blogs">
-          <h2 className="dummy">
-            Popular Blogs
-          </h2>
+          <h2 className="dummy">Popular Blogs</h2>
 
           <div className={Style.blog}>
             <h1 className={`text-2xl font-semibold text-center`}>
